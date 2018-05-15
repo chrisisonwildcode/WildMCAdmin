1de64ac7bbb9996d8a65e116c3b1abd16fc421d2
Chris Ison <chrisisonwildcode@gmail.com>
2018-05-15 18:15:12 +1000

    Initial commit

---
+++ src/com/wildcode/wildmcadmin/WildMCAdmin.java
@@ -0,0 +1,15 @@
+package com.wildcode.wildmcadmin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WildMCAdmin extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+}
